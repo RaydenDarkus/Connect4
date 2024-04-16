457685d676f6400a7b759a77bef602b9c9368f19
RaydenDarkus <svpatil20000@gmail.com>
2024-04-16 19:03:53 -0400

    New Commit

--- client/src/constants/constants.jsx
+++ client/src/constants/constants.jsx
@@ -1,3 +1,3 @@
 export const size = 100
-export const row = 6
-export const col = 7
+export const rows = 6
+export const columns = 7
---
+++ client/src/constants/consts.jsx
@@ -0,0 +1,9 @@
+export const EVAL_WEIGHTS = { FOURS: 10, THREES: 5, TWOS: 2 }
+  
+export const EASY = 2
+export const MEDIUM = 5
+export const HARD = 7
+  
+export const MAX_VALUE = Number.MAX_VALUE
+export const MIN_VALUE = -Number.MAX_VALUE
+  
